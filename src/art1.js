697db689ac04a3b83eb0482b75952de79de35edb
Sher Minn Chong <sherminn.chong@gmail.com>
2019-05-19 17:47:35 -0400

    Clean up tests

--- src/art1.js
+++ src/art1.js
@@ -13,6 +13,10 @@ function fill(arr, symbol, ncol) {
   return arr.map(row => row.map((char, j) => (j % ncol === 0 ? symbol : " ")));
 }
 
+function stringify(arr) {
+  return arr.map(row => row.join("")).join("\n");
+}
+
 function init({
   symbol1 = "",
   ncol = 1,
@@ -22,26 +26,22 @@ function init({
   width = CANVAS_WIDTH,
   height = CANVAS_HEIGHT
 }) {
-  const a1 = fill(array(width, height), symbol1, ncol);
-  const a2 = fill(array(width, height), symbol2, mcol);
+  const arr1 = fill(array(width, height), symbol1, ncol);
+  const arr2 = fill(array(width, height), symbol2, mcol);
 
-  return { a1, a2, title };
-}
-
-function stringify(a) {
-  return a.map(row => row.join("")).join("\n");
+  return { arr1, arr2, title };
 }
 
 function print(canvas) {
   const { a1, a2 } = canvas;
   return `${stringify(a1)}\n\n${stringify(a2)}`;
 }
+function ellipse(arr) {}
 
 // function line(canvas) {}
 // function rectSolid(canvas) {}
 // function rectOpen(canvas) {}
 // function triangle(canvas) {}
-// function ellipse(canvas) {}
 // function quadrants(canvas) {}
 // function exponential(canvas) {}
 
--- tests/art1.test.js
+++ tests/art1.test.js
@@ -1,51 +1,63 @@
-const { Art1, array, print, init, stringify } = require('../src/art1');
+const { array, init } = require("../src/art1");
 
-describe('array', () => {
-  it('should return 2D array of length', () => {
+describe("array", () => {
+  it("should return 2D array of length", () => {
     const c = array(10, 8);
-    expect(c.length).toBe(8); //num of rows
-    expect(c[0].length).toBe(10); //num of cols
+    expect(c.length).toBe(8); // num of rows
+    expect(c[0].length).toBe(10); // num of cols
   });
 });
 
-describe('init', () => {
-  it('should fill canvas', () => {
-    const c = init({width: 4,
-      height: 3,
-      symbol1: '-',
-      ncol: 1,
-      symbol2: '"',
-      mcol: 1});
-
-    const pattern1 = '----'
-    const pattern2 = '""""'
-    expect(c.a1).toEqual([
-      pattern1.split(''),
-      pattern1.split(''),
-      pattern1.split('')]);
-    expect(c.a2).toEqual([
-      pattern2.split(''),
-      pattern2.split(''),
-      pattern2.split('')]);
+describe("init", () => {
+  const setup = options =>
+    init(
+      Object.assign(
+        {
+          width: 4,
+          height: 3,
+          symbol1: "-",
+          ncol: 1,
+          symbol2: '"',
+          mcol: 1
+        },
+        options
+      )
+    );
+  it("should fill canvas", () => {
+    const c = setup();
+    const pattern1 = "----";
+    const pattern2 = '""""';
+    expect(c.arr1).toEqual([
+      pattern1.split(""),
+      pattern1.split(""),
+      pattern1.split("")
+    ]);
+    expect(c.arr2).toEqual([
+      pattern2.split(""),
+      pattern2.split(""),
+      pattern2.split("")
+    ]);
   });
 
-  it('should fill canvas with alternating', () => {
-    const c = init({width: 4,
-      height: 3,
-      symbol1: '-',
-      ncol: 2,
-      symbol2: 'x',
-      mcol: 3});
+  it("should fill canvas with alternating", () => {
+    const c = setup({ symbol1: "-", ncol: 2, symbol2: "x", mcol: 3 });
 
-    const pattern1 = '- - '
-    const pattern2 = 'x  x'
-    expect(c.a1).toEqual([
-      pattern1.split(''),
-      pattern1.split(''),
-      pattern1.split('')]);
-    expect(c.a2).toEqual([
-      pattern2.split(''),
-      pattern2.split(''),
-      pattern2.split('')]);
+    const pattern1 = "- - ";
+    const pattern2 = "x  x";
+    expect(c.arr1).toEqual([
+      pattern1.split(""),
+      pattern1.split(""),
+      pattern1.split("")
+    ]);
+    expect(c.arr2).toEqual([
+      pattern2.split(""),
+      pattern2.split(""),
+      pattern2.split("")
+    ]);
   });
+});
+
+describe("line", () => {
+  it("should draw vertical line", () => {});
+  it("should draw horizontal line", () => {});
 });
