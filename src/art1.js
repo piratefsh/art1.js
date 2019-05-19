3bb5daa68008ca561cd8d3b9a7e90860f1944825
Sher Minn Chong <sherminn.chong@gmail.com>
2019-05-19 17:19:58 -0400

    lint

--- src/art1.js
+++ src/art1.js
@@ -1,44 +1,49 @@
 const CANVAS_WIDTH = 105;
 const CANVAS_HEIGHT = 50;
 
-function Art1(){
+function Art1() {
   return true;
 }
 
-function array(w=CANVAS_WIDTH, h=CANVAS_HEIGHT){
-  return new Array(h).fill(null).map(()=>new Array(w).fill(null));
+function array(w = CANVAS_WIDTH, h = CANVAS_HEIGHT) {
+  return new Array(h).fill(null).map(() => new Array(w).fill(null));
 }
 
-function fill(array, symbol, ncol) {
-  return array.map((row, i) => row.map(
-    (char, j) => (
-      j % ncol == 0 ? symbol : " "
-    )))
+function fill(arr, symbol, ncol) {
+  return arr.map(row => row.map((char, j) => (j % ncol === 0 ? symbol : " ")));
 }
 
-function init({ symbol1="", ncol=1, symbol2="", mcol=1, title, width=CANVAS_WIDTH, height=CANVAS_HEIGHT }){
+function init({
+  symbol1 = "",
+  ncol = 1,
+  symbol2 = "",
+  mcol = 1,
+  title,
+  width = CANVAS_WIDTH,
+  height = CANVAS_HEIGHT
+}) {
   const a1 = fill(array(width, height), symbol1, ncol);
   const a2 = fill(array(width, height), symbol2, mcol);
 
-  return {a1, a2};
+  return { a1, a2, title };
 }
 
-function stringify (a) {
-  return a.map((row) => row.join("")).join("\n");
+function stringify(a) {
+  return a.map(row => row.join("")).join("\n");
 }
 
-function print(canvas){
-  const {a1, a2} = canvas;
-  return `${stringify(a1)}\n\n${stringify(a2)}`
+function print(canvas) {
+  const { a1, a2 } = canvas;
+  return `${stringify(a1)}\n\n${stringify(a2)}`;
 }
 
-function line(canvas){}
-function rectSolid(canvas){}
-function rectOpen(canvas){}
-function triangle(canvas){}
-function ellipse(canvas){}
-function quadrants(canvas){}
-function exponential(canvas){}
+// function line(canvas) {}
+// function rectSolid(canvas) {}
+// function rectOpen(canvas) {}
+// function triangle(canvas) {}
+// function ellipse(canvas) {}
+// function quadrants(canvas) {}
+// function exponential(canvas) {}
 
 module.exports = {
   Art1,
