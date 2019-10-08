a557ad18a0489a2ec1d25af83fb4f98c07a94492
Hugh Pyle <hpyle@cabezal.com>
2019-10-08 08:11:05 -0400

    Tidy up tiddlywinks

--- examples/tiddly_winks.js
+++ examples/tiddly_winks.js
@@ -3,7 +3,7 @@ const { init, rect, ellipse, oprint } = require("../src/art1");
 // after Frederick Hammersley "Tiddly Winks" 1969
 
 (function main() {
-  const width = 72;
+  const width = 71;
   const height = 38;
   const cx = width / 2;
   const cy = height / 2;
@@ -19,9 +19,9 @@ const { init, rect, ellipse, oprint } = require("../src/art1");
     mcol: 1
   });
 
-  const d1 = (rx-1) * 1 + 1;
-  const d2 = (rx-2) * 2 + 1;
-  const d3 = (rx-2) * 3 + 1;
+  const d1 = Math.floor(rx)-2;
+  const d2 = d1 * 2;
+  const d3 = d1 * 3;
 
   c.arr2 = ellipse(c.arr2, { symbol: "-", r: ry,              c: rx, nr: ry, nc: rx });
   c.arr2 = ellipse(c.arr2, { symbol: "-", r: height - ry - 1, c: width - rx - 2, nr: ry, nc: rx });
