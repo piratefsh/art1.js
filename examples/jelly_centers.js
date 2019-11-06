8d9384594394680b77bf7f634a4aafbd0cfb64f7
Hugh Pyle <hpyle@cabezal.com>
2019-11-06 18:05:36 -0500

    update jelly-centers to fit on tty page

--- examples/jelly_centers.js
+++ examples/jelly_centers.js
@@ -6,8 +6,8 @@ const { init, rect, ellipse, oprint } = require("../src/art1");
   const gx = 3;
   const gy = 4;
   const cy = 12;  // height of each block
-  const cx = 24;  // slightly different than original because we're only 72 wide in total
-  const width = 72;
+  const cx = 24;  // width of each block - slightly different than original because we're only 72 wide in total
+  const width = 71;
   const height = gy * cy;
 
   const c = init({
@@ -17,7 +17,7 @@ const { init, rect, ellipse, oprint } = require("../src/art1");
     ncol: 1,
     symbol2: "'",
     mcol: 1,
-    title: " ".repeat(29) + "JELLY  CENTERS"
+    title: " ".repeat(29) + "JELLY CENTERS"
   });
 
   for (let i = 0; i < gy + 1; i += 1) {
@@ -26,7 +26,7 @@ const { init, rect, ellipse, oprint } = require("../src/art1");
       c.arr1 = ellipse(c.arr1, {
         symbol: " ",
         r: cy * i,
-        c: cx * j,
+        c: cx * j - 1,
         nr: 6,
         nc: 12
       });
@@ -34,7 +34,7 @@ const { init, rect, ellipse, oprint } = require("../src/art1");
       c.arr1 = ellipse(c.arr1, {
         symbol: "O",
         r: cy * i,
-        c: cx * j,
+        c: cx * j - 1,
         nr: 4,
         nc: 8
       });
@@ -43,7 +43,7 @@ const { init, rect, ellipse, oprint } = require("../src/art1");
         c.arr2 = rect(c.arr2, {
           symbol: ".",
           r: cy * i,
-          c: cx * j,
+          c: cx * j - 1,
           nr: cy,
           nc: cx
         });
